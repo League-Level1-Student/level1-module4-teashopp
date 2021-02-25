2dc6df5ab290114ca872d473f0094685f3614813
ethan <ethan@LAPTOP-FHOLCKVQ.lan>
2021-02-24 16:53:23 -0800

    24/02

--- src/_12_slot_machine/slot_machine.java
+++ src/_12_slot_machine/slot_machine.java
@@ -7,21 +7,32 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
+import javax.swing.AbstractButton;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class slot_machine {
+public class slot_machine implements ActionListener {
 
 	// frames and stuff
-	static JFrame frame = new JFrame();
-	static JPanel panel = new JPanel();
-	static JButton spinbutton = new JButton();
-	static Random spin = new Random();
+	JFrame frame = new JFrame();
+	JPanel buttonpanel = new JPanel();
 
+	JPanel panel = new JPanel();
+	
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
+
+	JButton spinbutton = new JButton("Spin!");
+	
+	Random spin = new Random();
+
+	
 	// main
 	public static void main(String[] args) {
 
@@ -29,48 +40,99 @@ public class slot_machine {
 		sm.setup();
 
 	}
+	
 
 	// set up frame
 	public void setup() {
 		
-		frame.setVisible(true);
-		frame.setSize(500, 300);
+		panel.setBounds(0, 100, 300, 300);		
+		buttonpanel.setBounds(550, 0, 60, 40);
 
+		frame.setVisible(true);
+		frame.setSize(1100, 500);
+		frame.add(buttonpanel);
 		frame.add(panel);
+		
+		panel.add(label1);
+		panel.add(label2);
+		panel.add(label3);
 		panel.add(spinbutton);
-		spinbutton.addActionListener((ActionListener) this);
+		
+		spinbutton.addActionListener(this);
 
 	}
 
 	// clicking button
-	void actionPerformed(ActionEvent e) {
-		
+	public void actionPerformed(ActionEvent e) {
+
 		spin();
-		
+		frame.pack();
+
 	}
 
 	// spin
 	public void spin() {
 
+		// panel 1
 		int newspin = spin.nextInt(3);
-		
+
 		if (newspin == 0) {
-			
-			
+			label1.setIcon(createLabelImage("cherry.jpg"));
+		}
+		
+		else if (newspin == 1) {
+			label1.setIcon(createLabelImage("lime.jpg"));
+		}
+		
+		else {
+			label1.setIcon(createLabelImage("orange.jpg"));
+		}
+		
+		
+		// panel 2
+		int newspin2 = spin.nextInt(3);
+		
+		if(newspin2 == 0) {
+			label2.setIcon(createLabelImage("cherry.jpg"));
+		}
+		
+		else if (newspin2 == 1) {
+			label2.setIcon(createLabelImage("lime.jpg"));
+		}
+		
+		else {
+			label2.setIcon(createLabelImage("orange.jpg"));
+		}
+		
+		
+		// panel 3
+		int newspin3 = spin.nextInt(3);
+		
+		if(newspin3 == 0) {
+			label3.setIcon(createLabelImage("cherry.jpg"));
+		}
+		
+		else if (newspin == 1) {
+			label3.setIcon(createLabelImage("lime.jpg"));
+		}
+		
+		else {
+			label3.setIcon(createLabelImage("orange.jpg"));
+		}
+		
+		
+		// you win message
+		if(newspin == 0 && newspin2 == 0 && newspin3 == 0 || newspin == 1 && newspin2 == 1 && newspin3 == 1 || newspin == 2 && newspin2 == 2 && newspin == 2) {
+			JOptionPane.showMessageDialog(null, "You win!");
 		}
 
 	}
 
 	// preset
-	private JLabel createLabelImage(String fileName) throws MalformedURLException {
+	private Icon createLabelImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
-		if (imageURL == null) {
-			System.err.println("Could not find image " + fileName);
-			return new JLabel();
-		}
 		Icon icon = new ImageIcon(imageURL);
-		JLabel imageLabel = new JLabel(icon);
-		return imageLabel;
+		return icon;
 	}
 
 }
